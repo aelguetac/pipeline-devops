478049662201d68eea0ad05a365e9ad68855ffe0
Alejandro Elgueta <alejandro.elgueta@gmail.com>
2021-01-03 19:36:53 -0300

    1

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -3,45 +3,45 @@ def call(){
 	def cstage = params.stage.split(';')
 	for (int i = 0 ; i < cstage.length; i++){
 							echo " el primer for para ${cstage[i]}"
-//	switch("cstage"){
-//	case "build":
+	switch(${cstage[i]}){
+	case "build":
 			stage('build') {
 //                      when { expression { "env.cstage == 'build' }}
 //			when { expression { ${cstage[i]} == 'build' }}
-			when { expression { "${params.stage}" == "build" }}
+//			when { expression { "${params.stage}" == "build" }}
 			env.TAREA = env.STAGE_NAME
                         sh "./mvn clean compile"
 
 			}
-//        break
-//	case "sonar":
+        break
+	case "sonar":
                         stage('sonar') {
                         env.TAREA = env.STAGE_NAME
                         def scannerHome = tool 'sonar_scanner';
 
                         withSonarQubeEnv('sonar') {
-//                                      sh "${scannerHome}/bin/sonar-scanner  -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+                                      sh "${scannerHome}/bin/sonar-scanner  -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
         bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
 
                                         }
 
                         }
-//	break
-//	case "run":
+	break
+	case "run":
                         stage('Run'){
                         env.TAREA = env.STAGE_NAME
                         sh 'nohup mvn spring-boot:run &'
                         }
-//	break
-//	case "test":
+	break
+	case "test":
                         stage('Test'){
                         env.TAREA = env.STAGE_NAME
                         sh 'sleep 30'
                         sh "curl -X GET 'http://localhost:8087/rest/mscovid/test?msg=testing'"
 
                         }
-//	break
-//	case "nexus":
+	break
+	case "nexus":
                         stage('Nexus'){
                         env.TAREA = env.STAGE_NAME
                         echo 'Testing failed!'
@@ -50,7 +50,7 @@ def call(){
                 packages: [[ $class: 'MavenPackage', MavenAssetList: [[classifier: 'RELEASE', extensions: 'jar' ,
                 filePath: './nada/build/libs/DevOpsUsach2020-0.0.1.jar']],
                 mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging : 'jar', version: '0.0.1']]]
-//	break
+	break
 
                         }
 
