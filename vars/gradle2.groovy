ce79a6a67e2623260315bf452a2613aaca60aa8a
Alejandro Elgueta <alejandro.elgueta@gmail.com>
2021-01-04 14:35:28 -0300

    1

--- vars/gradle2.groovy
+++ vars/gradle2.groovy
@@ -28,7 +28,7 @@ def call(){
                         stage('test'){
                         env.TAREA = env.STAGE_NAME
 			echo "Dentro de stage test"
-                        bat 'sleep 30'
+                        bat 'waitfor dir /t 20'
 			final String url = "http://localhost:8087/rest/mscovid/test?msg=testing"
                     	final String response = sh(script: "curl -s $url", returnStdout: true).trim()
                     	echo response
